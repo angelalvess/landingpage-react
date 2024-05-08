1384be0c4f4e78498ea176b96a08601e7b886c3d
angie <angeltrashangel@gmail.com>
2024-05-08 05:45:06 -0300

    feat: finish project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Pricing from './components/Pricing/Pricing';
+import Testimonials from './components/Testimonials/Testimonials';
+import Workflow from './components/Workflow/Workflow';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
